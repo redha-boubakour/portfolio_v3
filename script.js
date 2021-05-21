d6a2f9011c62d11b03e59fd0050ff14e92d9fae1
Redha <redha.boubakour@yahoo.fr>
2021-05-21 22:47:47 +0200

    Testing - IntersectionObserver

--- script.js
+++ script.js
@@ -1,54 +1,61 @@
-const navLinkHome = document.querySelector('.nav__link--home');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
-const navLinkProjects = document.querySelector('.nav__link--projects');
-const navLinkJourney = document.querySelector('.nav__link--journey');
-const navLinkContact = document.querySelector('.nav__link--contact');
 
-const navLinkActive = document.querySelector('.nav__link--btn');
-
-const header = document.querySelector('#header');
 const nav = document.querySelector('.nav');
+const sections = document.querySelectorAll('section');
 const section1 = document.querySelector('#section--1');
-const section2 = document.querySelector('#section--2');
-const section3 = document.querySelector('#section--3');
+const navLinks = document.querySelectorAll('.nav__link');
 
+// On click, Allow the nav links to go to their respectives sections
 
-navLinkHome.addEventListener('click', function(e) {
-    header.scrollIntoView({ behavior: 'smooth' });
-});
+const makeNavLinksSmooth = () => {
+    
+    for (let n in navLinks) {
+        if (navLinks.hasOwnProperty(n)) {
+            navLinks[n].addEventListener('click', e => {
+                e.preventDefault();
+                document.querySelector(navLinks[n].hash).scrollIntoView({ 
+                    behavior: 'smooth' 
+                });
+            });
+        }
+    }
+}
+
+makeNavLinksSmooth();
+
+// Allow the 'My achievements ? ↓' button to lead smoothly to 'the projects' section
 
 btnScrollTo.addEventListener('click', function(e) {
     section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-navLinkProjects.addEventListener('click', function(e) {
-    section1.scrollIntoView({ behavior: 'smooth' });
-});
-
-navLinkJourney.addEventListener('click', function(e) {
-    section2.scrollIntoView({ behavior: 'smooth' });
-});
-
-navLinkContact.addEventListener('click', function(e) {
-    section3.scrollIntoView({ behavior: 'smooth' });
-});
-
-/*
-window.addEventListener('scroll', function () {
-    nav.classList.toggle('nav__sticky', window.scrollY > 0)
-});
-*/
-
-/////////////////////////////
+// Related to the stickiness of the nav bar
 
 const stickyNav = function (entries) {
-        const [entry] = entries;
+    entries.forEach(entry => {
         if (!entry.isIntersecting) nav.classList.add('nav__sticky');
         else nav.classList.remove('nav__sticky');
+    });
 };
 
 const headerObserver = new IntersectionObserver(stickyNav, {
-    root: null,
     threshold: 1
 });
+
 headerObserver.observe(header);
+
+// Related to the nav links sticking with the sections in the viewport
+
+const navLinkCheck = function (entries) {
+    entries.forEach(entry => {
+
+    });
+};
+
+let sectionsObserver = new IntersectionObserver(navLinkCheck, {
+    threshold: 0.7
+});
+
+sections.forEach(section => {
+    sectionsObserver.observe(section);
+});
